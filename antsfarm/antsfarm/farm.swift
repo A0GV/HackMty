b743d5438b256af60455e00a74fd7581e8d65f96
Alejandro Uribe <a01741542@tec.mx>
2025-10-25 12:44:21 -0600

    game with text
    still missing the ants and the movement of the ant

--- antsfarm/antsfarm/farm.swift
+++ antsfarm/antsfarm/farm.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct farm: View {
     var body: some View {
+        //Fondo
         ZStack {
             GeometryReader { geometry in
                 VStack(spacing: 0) {
@@ -27,7 +28,50 @@ struct farm: View {
             }
             .ignoresSafeArea(edges: [.top, .leading, .trailing])
             
-            // Your content goes here
+            VStack {
+                VStack(alignment: .leading, spacing: 20) {
+                    HStack {
+                        Text("My farm")
+                            .font(.system(size: 30))
+                            .bold()
+                            .padding(5)
+                            .background(Color.white)
+                            .cornerRadius(10)
+                            .foregroundStyle(CategoryColors.principal)
+                        
+                        Spacer()
+                        
+                        HStack(alignment: .center) {
+                            Image("hoja")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 35)
+                            Text("15")
+                                .font(.system(size: 30))
+                                .bold()
+                                .foregroundStyle(CategoryColors.principal)
+                        }
+                        .padding(5)
+                        .background(Color.white)
+                        .cornerRadius(10)
+                    }
+                    
+                    Button(action:{}){
+                        Text("Spin roulette")
+                            .font(.system(size: 30))
+                            .bold()
+                            .foregroundStyle(Color.white)
+                    }.padding(5).background(CategoryColors.principal)
+                        .cornerRadius(10)
+                    
+                    Spacer()
+                }
+                .padding(30)
+                .padding(.top, 20)
+                
+                Spacer()
+            }
+
         }
     }
 }
