4331a9db0555687811c2e75515db4e5730b68de1
Alejandro Uribe <a01741542@tec.mx>
2025-10-25 10:10:32 -0600

    swiftui

---
+++ antsfarm/antsfarm/antsfarmApp.swift
@@ -0,0 +1,17 @@
+//
+//  antsfarmApp.swift
+//  antsfarm
+//
+//  Created by Alejandro Uribe Moreno on 25/10/25.
+//
+
+import SwiftUI
+
+@main
+struct antsfarmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
