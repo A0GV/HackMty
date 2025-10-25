d6388ab727f372fadb903546e3f93d28b4f28442
Alejandro Uribe <a01741542@tec.mx>
2025-10-25 10:46:02 -0600

    adding pages

---
+++ antsfarm/antsfarm/farm.swift
@@ -0,0 +1,18 @@
+//
+//  farm.swift
+//  antsfarm
+//
+//  Created by Alejandro Uribe Moreno on 25/10/25.
+//
+
+import SwiftUI
+
+struct farm: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    farm()
+}
